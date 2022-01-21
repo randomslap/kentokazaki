2b1c165a9f7d03811c07bbcb2fa1feddf91b4e90
unknown <kentokazaki@gmail.com>
2022-01-20 22:58:12 -0800

    update a tags

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -8,4 +8,17 @@ export default createGlobalStyle`
     h1, h2, h3, p {
         font-family: 'Ubuntu', sans-serif;
     }
+
+    a:link {
+        text-decoration: none !important; 
+    }
+    a:hover { 
+        text-decoration: none !important;
+    }
+    a:active { 
+        text-decoration: none !important; 
+    }
+    a:visited { 
+        text-decoration: none !important; 
+    }
 `
