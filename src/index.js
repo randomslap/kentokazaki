6763cd89b303faa02caf020dca32e5a1051ecaf6
randomslap <kentokazaki@gmail.com>
2023-05-18 21:05:54 -0700

    added vercel analytics

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 
@@ -6,41 +7,42 @@ import './index.css'
 import App from './App'
 
 const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      light: '#00bcd4',
-      main: '#00bcd4',
-      dark: '#00bcd4',
+    palette: {
+        mode: 'dark',
+        primary: {
+            light: '#00bcd4',
+            main: '#00bcd4',
+            dark: '#00bcd4',
+        },
+        secondary: {
+            light: '#00bcd4',
+            main: '#00bcd4',
+            dark: '#00bcd4',
+        },
+        text: {
+            primary: '#fff',
+        },
     },
-    secondary: {
-      light: '#00bcd4',
-      main: '#00bcd4',
-      dark: '#00bcd4',
+    typography: {
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
     },
-    text: {
-      primary: '#fff',
-    },
-  },
-  typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  },
 })
 
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={darkTheme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+    <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+        <Analytics />
+    </ThemeProvider>,
 )
